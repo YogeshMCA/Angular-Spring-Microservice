268131dec4f8b8c05b35f9e8c11df694688a8f28
yogesh <yogee1389@gmail.com>
2019-09-06 21:18:55 +0530

    Delete - Completed

---
+++ src/app/feedback/search/response-message.ts
@@ -0,0 +1,3 @@
+export class ResponseMessage {
+    public messgae: String;
+}
--- src/app/feedback/search/search.component.ts
+++ src/app/feedback/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserFeedback } from '../user-feedback';
 import { HttpClient } from '@angular/common/http';
+import { ResponseMessage } from './response-message';
 
 @Component({
   selector: 'app-search',
@@ -14,6 +15,7 @@ export class SearchComponent implements OnInit {
   selectedValue: String;
   userFeedbk: UserFeedback[];
   userFeedbk1: UserFeedback;
+  responseMessage: ResponseMessage;
 
   constructor(private http: HttpClient) {
     this.fieldLst = ["ID","Name","Email"];
@@ -32,7 +34,9 @@ export class SearchComponent implements OnInit {
   }
 
   deleteDetail(usrFeedback: UserFeedback){
-    this.http.get<String>('https://curr-conversion.cfapps.io/user-feedback-delete/id/'+usrFeedback.id);
+    this.http.get('https://curr-conversion.cfapps.io/user-feedback-delete/id/'+usrFeedback.id,{responseType:"text"}).
+    subscribe(data =>{console.log(usrFeedback);this.userFeedbk.splice(this.userFeedbk.indexOf(usrFeedback),1);});
+    
     
   }
 }
