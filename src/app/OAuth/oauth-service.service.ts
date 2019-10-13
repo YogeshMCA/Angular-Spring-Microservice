44bab67320f4be3798995997343277d005c0ed1b
yogesh <yogee1389@gmail.com>
2019-10-13 11:21:19 +0530

    OpenId Connect

--- src/app/OAuth/oauth-service.service.ts
+++ src/app/OAuth/oauth-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {UserManager,User} from 'oidc-client';
+import {UserManager,User, WebStorageStateStore} from 'oidc-client';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -7,17 +7,23 @@ import { HttpClient } from '@angular/common/http';
 export class OauthServiceService {
 
 private userManager: UserManager;
-
+private _user: User;
   constructor(private httpClient: HttpClient) {
 
     var config = {
       authority: 'https://dev-bedtkbb3.auth0.com',
       client_id: 'tqhIw7fr68SdztxJl3sKNGq145Ze7DMa',
-      redirect_uri: `${window.location.origin}/callback`,
+      redirect_uri: `http://localhost:4200/searchFeedback`,
       scope: 'openid profile',
-      response_type:'id_token token'
+      response_type:'id_token token',
+      userStore: new WebStorageStateStore({store:window.localStorage})
     };
     this.userManager = new UserManager(config);
+    this.userManager.getUser().then(user =>{
+      if(user && !user.expired){
+        this._user = user;
+      }
+    });
    }
 
    login(): Promise<any>{
